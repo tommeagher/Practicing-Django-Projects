d6de4cad2dd1a5da5c12d02bc4196d29b2fc87f9
Tom Meagher <car+bit@tommeagher.com>
2012-01-30 22:42:57 -0500

    add views for links, categories and tags

--- blurg/urls.py
+++ blurg/urls.py
@@ -1,19 +1,42 @@
 from django.conf.urls.defaults import *
 
-from blurg.models import Entry
+from blurg.models import Entry, Link, Category
+
+from tagging.models import Tag
 
 entry_info_dict = {
     'queryset': Entry.objects.all(),
     'date_field': 'pub_date',
 }
 
-urlpatterns = patterns('django.views.generic.date_based',
-   #url(r'^$', 'blurg.views.entries_index'),
-    url(r'^$', 'archive_index', entry_info_dict, 'blurg_entry_archive_index'),
-    url(r'^(?P<year>\d{4})/$', 'archive_year', entry_info_dict, 'blurg_entry_archive_year'),
-    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$', 'archive_month', entry_info_dict, 'blurg_entry_archive_month'),
-    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'archive_day', entry_info_dict, 'blurg_entry_archive_day'),
-    #url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'blurg.views.entry_detail'),
-    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', entry_info_dict, 'blurg_entry_detail'),    
+link_info_dict = {
+    'queryset': Link.objects.all(),
+    'date_field': 'pub_date',
+    }
 
-)
+urlpatterns = patterns('django.views.generic.date_based',
+    #(r'^$', 'blurg.views.entries_index'),
+    (r'^$', 'archive_index', entry_info_dict, 'blurg_entry_archive_index'),
+    (r'^(?P<year>\d{4})/$', 'archive_year', entry_info_dict, 'blurg_entry_archive_year'),
+    (r'^(?P<year>\d{4})/(?P<month>\w{3})/$', 'archive_month', entry_info_dict, 'blurg_entry_archive_month'),
+    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'archive_day', entry_info_dict, 'blurg_entry_archive_day'),
+    #(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'blurg.views.entry_detail'),
+    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', entry_info_dict, 'blurg_entry_detail'),    
+    (r'^links/$', 'archive_index', link_info_dict, 'blurg_link_archive_index'),
+    (r'^links/(?P<year>\d{4})/$', 'archive_year', link_info_dict, 'blurg_link_archive_year'),
+    (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/$', 'archive_month', link_info_dict, 'blurg_link_archive_month'),
+    (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'archive_day', link_info_dict, 'blurg_link_archive_day'),
+    (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', link_info_dict, 'blurg_link_detail'),
+    )
+
+urlspatterns += patterns('',
+    (r'^categories/^$', 'django.views.generic.list_detail.object_list', {'queryset': Category.objects.all() }),
+    (r'^categories/(?P<slug>[-\w]+)/$', 'coltrane.views.category_detail'),
+    )
+    
+urlpatterns += patterns('',
+    (r'^tags/$', 'django.views.generic.list_detail.object_list', ('queryset': Tag.objects.all() }),
+    (r'^tags/entries/(?P<tag>[-\w]+)/$', 'tagging.views.tagged_object_list', { 'queryset_or_model': Entry, 'template_name': 'blurg/entries_by_tag.html' }),
+    (r'^tags/links/(?P<tag>{-\w}+)/$', 'tagging.views.tagged_object_list', { 'queryset_or_model': Link, 'template_name': 'blurg/links_by_tag.html' }),    
+	)
+	
--- blurg/views.py
+++ blurg/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import get_object_or_404, render_to_response
-from blurg.models import Entry
+from blurg.models import Entry, Category
+from django.views.generic.list_detail import object_list
 
 def entries_index(request):
-    return render_to_response('blurg/entry_index.html', {'entry_list': Entry.objects.all() })
+    return render_to_response('blurg/entry_index.html', {'object_list': Entry.objects.all() })
+
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    return object_list(request, queryset=category.entry_set.all(), extra_context={ 'category': category })
 
 #made redundant by generic view    
+#def category_list(request):
+#    return render_to_response('blurg/category_list.html', {'object_list': Category.objects.all() })
+
+#def category_detail(request):
+#    category = get_object_or_404(Category, slug=slug)
+#    return render_to_response('blurg/category_detail.html', {'object_list': category.entry_set.all(), 'category': category'})
+    
 #def entry_detail(request, year, month, day, slug):
 #    import datetime, time
 #    date_stamp = time.strptime(year+month+day, "%Y%b%d")
