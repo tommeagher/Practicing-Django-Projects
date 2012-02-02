a5d6f84cff30bb45eb362a8b91bf42aee0ecd97e
Tom Meagher <car+bit@tommeagher.com>
2012-02-01 22:24:50 -0500

    created base.html template

--- blurg/urls/categories.py
+++ blurg/urls/categories.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 from blurg.models import Category
 
-urlspatterns = patterns('',
+urlpatterns = patterns('',
     (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Category.objects.all() }),
-    (r'^(?P<slug>[-\w]+)/$', 'coltrane.views.category_detail'),
+    (r'^(?P<slug>[-\w]+)/$', 'blurg.views.category_detail'),
 )
--- blurg/urls/tags.py
+++ blurg/urls/tags.py
@@ -4,7 +4,7 @@ from blurg.models import Entry, Link
 from tagging.models import Tag
     
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.list_detail.object_list', ('queryset': Tag.objects.all() }),
+    (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Tag.objects.all() }),
     (r'^entries/(?P<tag>[-\w]+)/$', 'tagging.views.tagged_object_list', { 'queryset_or_model': Entry.live.all(), 'template_name': 'blurg/entries_by_tag.html' }),
     (r'^links/(?P<tag>{-\w}+)/$', 'tagging.views.tagged_object_list', { 'queryset_or_model': Link, 'template_name': 'blurg/links_by_tag.html' }),    
 )
