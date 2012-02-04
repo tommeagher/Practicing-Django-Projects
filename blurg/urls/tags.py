369162ca5f9fc91debc2081d439b34e5ad898622
Tom Meagher <car+bit@tommeagher.com>
2012-02-03 21:56:12 -0500

    finished roughing out blurg templates

--- blurg/urls/tags.py
+++ blurg/urls/tags.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Tag.objects.all() }),
     (r'^entries/(?P<tag>[-\w]+)/$', 'tagging.views.tagged_object_list', { 'queryset_or_model': Entry.live.all(), 'template_name': 'blurg/entries_by_tag.html' }),
     (r'^links/(?P<tag>{-\w}+)/$', 'tagging.views.tagged_object_list', { 'queryset_or_model': Link, 'template_name': 'blurg/links_by_tag.html' }),    
+    url(r'^(?P<tag>[^/]+)/$', 'blurg.views.tag_detail', 'template_name'='blurg/tag_detail.html'),
 )
 	
--- blurg/views.py
+++ blurg/views.py
@@ -10,6 +10,47 @@ def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
     return object_list(request, queryset=category.live_entry_set.all(), extra_context={ 'category': category })
 
+# From Ben Welsh's excellent palewire.com code
+def tag_detail(request, tag):
+    """
+    A list that reports all of the content with a particular tag.
+    """
+    try:
+        tag = Tag.objects.get(name=tag)
+    except Tag.DoesNotExist:
+        # If the tag isn't found, try it with hyphens removed, which
+        # was the convention on my old Wordpress blog. This can help
+        # keep old tag page links alive.
+        try:
+            tag = Tag.objects.get(name=tag.replace("-", ""))
+        except Tag.DoesNotExist:
+            raise Http404
+    
+    # Pull all the items with that tag.
+    taggeditem_list = TaggedItem.objects.filter(tag=tag).fetch_generic_relations()
+    
+    # Loop through the tagged items and return just the items
+    object_list = [i.object for i in taggeditem_list if getattr(i.object, 'pub_date', False)]
+    
+    # Now resort them by the pub_date attribute we know each one should have
+    object_list.sort(key=lambda x: x.pub_date, reverse=True)
+    
+    # Slice it to 500 max
+    object_list = object_list[:500]
+
+    if len(object_list) == 500:
+        maxed_out = True
+    else:
+        maxed_out = False
+
+    # Pass it out
+    return render(request, 'blurg/tag_detail.html', { 
+            'tag': tag, 
+            'object_list': object_list,
+            'maxed_out': maxed_out,
+        })
+
+
 #made redundant by generic view    
 #def category_list(request):
 #    return render_to_response('blurg/category_list.html', {'object_list': Category.objects.all() })
