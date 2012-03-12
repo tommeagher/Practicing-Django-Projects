4c2a138693d4b7bca6dae95086aef1f24353f505
Tom Meagher <car+bit@tommeagher.com>
2012-03-11 21:15:39 -0400

    squashing bugs in Chapter 6

--- blurg/templatetags/blurg_tags.py
+++ blurg/templatetags/blurg_tags.py
@@ -1,27 +1,28 @@
-#from django.db.models import get_model
-#from django import template
+from django.db.models import get_model
+from django import template
+from blurg.models import Entry
 
-#def do_latest_content(parser, token):
-#    bits = token.split_contents()
-#    if len(bits) != 5:
-#        raise template.TemplateSyntaxError("'get_latest_content' tag takes exactly four arguments")
-#    model_args = bits[1].split('.')
-#    if len(model_args) != 2:
-#        raise.TemplateSyntaxError("First argument to 'get_latest_content' must be an 'application name'.'model name' string")
-#    model = get_model(*model_args)
-#    if model is None:
-#        raise template.TemplateSyntaxError("'get_latest_content' tag got an invalid model: %s" % bits[1])
-#    return LatestContentNode(model, bits[2], bits[4])
+def do_latest_content(parser, token):
+    bits = token.split_contents()
+    if len(bits) != 5:
+        raise template.TemplateSyntaxError("'get_latest_content' tag takes exactly four arguments")
+    model_args = bits[1].split('.')
+    if len(model_args) != 2:
+        raise template.TemplateSyntaxError("First argument to 'get_latest_content' must be an 'application name'.'model name' string")
+    model = get_model(*model_args)
+    if model is None:
+        raise template.TemplateSyntaxError("'get_latest_content' tag got an invalid model: %s" % bits[1])
+    return LatestContentNode(model, bits[2], bits[4])
     
-#class LatestContentNode(template.Node):
-#    def __init__(self, model, num, varname):
-#        self.model = model
-#        self.num = int(num)
-#        self.varname = varname
-#    
-#    def render(self, context):
-#        context[self.varname] = self.model._default_manager.all()[:self.num]
-#        return ''
+class LatestContentNode(template.Node):
+    def __init__(self, model, num, varname):
+        self.model = model
+        self.num = int(num)
+        self.varname = varname
     
-#register = template.Library()
-#register.tag('get_latest_content', do_latest_content)
+    def render(self, context):
+        context[self.varname] = self.model._default_manager.all()[:self.num]
+        return ''
+    
+register = template.Library()
+register.tag('get_latest_content', do_latest_content)
--- blurg/urls/tags.py
+++ blurg/urls/tags.py
@@ -4,9 +4,9 @@ from blurg.models import Entry, Link
 from tagging.models import Tag
     
 urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Tag.objects.all() }),
+    (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Tag.objects.all(), 'template_name': 'blurg/tag_list.html'}, 'blurg_tag_list'),
     (r'^entries/(?P<tag>[-\w]+)/$', 'tagging.views.tagged_object_list', { 'queryset_or_model': Entry.live.all(), 'template_name': 'blurg/entries_by_tag.html' }),
     (r'^links/(?P<tag>{-\w}+)/$', 'tagging.views.tagged_object_list', { 'queryset_or_model': Link, 'template_name': 'blurg/links_by_tag.html' }),    
-    url(r'^(?P<tag>[^/]+)/$', 'blurg.views.tag_detail', 'template_name'='blurg/tag_detail.html'),
+    #url(r'^(?P<tag>[^/]+)/$', 'blurg.views.tag_detail', 'template_name'='blurg/tag_detail.html'),
 )
 	
