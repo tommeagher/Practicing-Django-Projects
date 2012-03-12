1c0f7d89acf02625a6771b85ed0a5086d49951ef
Tom Meagher <car+bit@tommeagher.com>
2012-03-11 22:17:44 -0400

    removed old blurg file names

--- blurg/urls/categories.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-from blurg.models import Category
-
-urlpatterns = patterns('',
-    (r'^$', 'django.views.generic.list_detail.object_list', {'queryset': Category.objects.all() }, 'blurg_category_list'),
-    (r'^(?P<slug>[-\w]+)/$', 'blurg.views.category_detail', {}, 'blurg_category_detail'),
-)
